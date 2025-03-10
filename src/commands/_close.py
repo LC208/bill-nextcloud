6d79ca56808dd79a4e8173408c3296ff09b2a881
LC208 <maks.2.03@mail.ru>
2025-03-10 07:00:16 +0000

    adbase close realization

--- src/commands/_close.py
+++ src/commands/_close.py
@@ -1,2 +1,11 @@
-def close(item: int, running_operation: int) -> None:
-    pass
+from utils.api import NextCloudAPI
+import billmgr.misc as misc
+from pmnextcloud import LOGGER
+
+
+def close(item: int) -> None:
+    api = NextCloudAPI.from_item(item)
+    if api.delete_user(f"user_{item}") is None:
+        LOGGER.error("Can't delete user in NextCloud")
+        raise Exception("Error on Nextcloud side")
+    misc.postclose(item)
