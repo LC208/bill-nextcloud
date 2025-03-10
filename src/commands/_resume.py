bc508c1814d51bd576b0d96cb8c2aa4127724d5f
LC208 <maks.2.03@mail.ru>
2025-03-10 07:00:31 +0000

    adbase resume realization

--- src/commands/_resume.py
+++ src/commands/_resume.py
@@ -1,2 +1,11 @@
+from utils.api import NextCloudAPI
+import billmgr.misc as misc
+from pmnextcloud import LOGGER
+
+
 def resume(item: int) -> None:
-    pass
+    api = NextCloudAPI.from_item(item)
+    if api.unsuspend_user(f"user_{item}") is None:
+        LOGGER.error("Can't disable user in NextCloud")
+        raise Exception("Error on Nextcloud side")
+    misc.postresume(item)
