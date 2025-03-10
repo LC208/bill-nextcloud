02852c3d12ebb9ec94bee9b705bbfe3bdb2b11d0
LC208 <maks.2.03@mail.ru>
2025-03-10 07:00:52 +0000

    suspend base realization

--- src/commands/_suspend.py
+++ src/commands/_suspend.py
@@ -1,2 +1,11 @@
+from utils.api import NextCloudAPI
+import billmgr.misc as misc
+from pmnextcloud import LOGGER
+
+
 def suspend(item: int) -> None:
-    pass
+    api = NextCloudAPI.from_item(item)
+    if api.suspend_user(f"user_{item}") is None:
+        LOGGER.error("Can't disable user in NextCloud")
+        raise Exception("Error on Nextcloud side")
+    misc.postsuspend(item)
