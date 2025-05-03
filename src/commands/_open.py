68bd928ea5579eebca6637829ff1fe60e4818b69
LC208 <maks.2.03@mail.ru>
2025-05-03 07:05:15 +0000

    испралена ошибка при выдаче группы,  изменена логика обработки ошибок

--- src/commands/_open.py
+++ src/commands/_open.py
@@ -7,19 +7,17 @@ from pmnextcloud import LOGGER
 
 
 def open(item: int) -> None:
-    api_client, user_service, group_service = (
-        CloudClientFactory.create_client_from_item(item)
-    )
+    api_client, user_service, group_service = CloudClientFactory.create_client_from_item(item)
     user = User(item, user_service)
+
     if user.exists:
         raise billmgr.exception.XmlException("open_colission_error")
+
     try:
         user_service.create_user(user.username, user.password, user.email, user.quota)
-    except:
-        LOGGER.error("Can't create user account")
-        raise billmgr.exception.XmlException("open_error")
-    try:
-        group_service.add_user_to_group(user.username, user.usergroup)
+        if user.usergroup != '':
+            group_service.add_user_to_group(user.username, user.usergroup)
+
         misc.save_param(user.item, param="username", value=user.username)
         if not user.exists:
             misc.save_param(
@@ -29,8 +27,13 @@ def open(item: int) -> None:
                 crypted=True,
             )
         misc.save_param(item, param="url", value=api_client.base_url)
-    except:
-        LOGGER.error("Can't save user account")
-        user_service.delete_user(user.username)
-    else:
-        misc.postopen(item)
+
+    except Exception as e:
+        LOGGER.error(f"Error during user setup: {e}")
+        try:
+            user_service.delete_user(user.username)
+        except Exception as cleanup_error:
+            LOGGER.error(f"Failed to cleanup user after error: {cleanup_error}")
+        raise billmgr.exception.XmlException("open_error")
+
+    misc.postopen(item)
